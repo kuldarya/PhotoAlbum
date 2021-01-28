a7a411c828b7bbed4c8c1ec2be394da425ae5c69
Darya Kuliashova <kuldiand@gmail.com>
2021-01-28 10:10:59 +0300

    First commit

---
+++ PhotoAlbum/AlbumsListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumsListViewController.swift
+//  PhotoAlbum
+//
+//  Created by Darya Kuliashova on 27.01.21.
+//
+
+import UIKit
+
+class AlbumsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
