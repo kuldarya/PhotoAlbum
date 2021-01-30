b5025fac273256fa290df24a3a34f628ea65e908
Darya Kuliashova <kuldiand@gmail.com>
2021-01-30 19:12:40 +0300

    photo delails ViewController

--- PhotoAlbum/TextConstants.swift
+++ PhotoAlbum/TextConstants.swift
@@ -9,6 +9,9 @@ import Foundation
 
 struct TextConstants {
     static let navigationAlbumTitle = "List of Albums"
+    static let navigationPhotosTitle = "List of Photos"
+    static let navigationPhotoDetailsTitle = "Photo Details"
+    static let cancel = "Cancel"
     
     static let basePath = "https://jsonplaceholder.typicode.com"
 }
